385db5beecdec1aef2fe9641c906411a6b2a8d58
ashishkhopde <ashishkhopde7089@gmail.com>
2026-05-03 16:02:30 +0530

    add dock section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Welcome } from '#components'
+import { Navbar, Welcome, Dock } from '#components'
 
 function App() {
 
@@ -6,6 +6,7 @@ function App() {
     <main>
       <Navbar />
       <Welcome />
+      <Dock/>
     </main>
   )
 }
